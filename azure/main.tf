4eb1cc27d73434565c50b39627e019f112565bc8
Jim Enright <jenright@cloudera.com>
2023-12-14 19:09:10 +0000

    Pin to latest release of pre-req modules and CDP provider version
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- azure/main.tf
+++ azure/main.tf
@@ -25,7 +25,7 @@ provider "azuread" {
 }
 
 module "cdp_azure_prereqs" {
-  source = "git::https://github.com/cloudera-labs/terraform-cdp-modules.git//modules/terraform-cdp-azure-pre-reqs?ref=v0.4.0"
+  source = "git::https://github.com/cloudera-labs/terraform-cdp-modules.git//modules/terraform-cdp-azure-pre-reqs?ref=v0.5.0"
 
   env_prefix   = var.env_prefix
   azure_region = var.azure_region
@@ -43,7 +43,7 @@ module "cdp_azure_prereqs" {
 }
 
 module "cdp_deploy" {
-  source = "git::https://github.com/cloudera-labs/terraform-cdp-modules.git//modules/terraform-cdp-deploy?ref=v0.4.0"
+  source = "git::https://github.com/cloudera-labs/terraform-cdp-modules.git//modules/terraform-cdp-deploy?ref=v0.5.0"
 
   env_prefix          = var.env_prefix
   infra_type          = "azure"
