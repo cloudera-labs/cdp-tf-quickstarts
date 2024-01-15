642e670252f90d33b335c1e73755b384315af173
Jim Enright <jenright@cloudera.com>
2024-01-15 09:57:31 +0000

    Create ssh keypair and lookup public ip for AWS if not specified
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- aws/main.tf
+++ aws/main.tf
@@ -23,7 +23,7 @@ module "cdp_aws_prereqs" {
   aws_region = var.aws_region
 
   deployment_template           = var.deployment_template
-  ingress_extra_cidrs_and_ports = var.ingress_extra_cidrs_and_ports
+  ingress_extra_cidrs_and_ports = local.ingress_extra_cidrs_and_ports
 
   # Using CDP TF Provider cred pre-reqs data source for values of xaccount account_id and external_id
   xaccount_account_id  = data.cdp_environments_aws_credential_prerequisites.cdp_prereqs.account_id
@@ -46,7 +46,7 @@ module "cdp_deploy" {
   env_prefix          = var.env_prefix
   infra_type          = "aws"
   region              = var.aws_region
-  keypair_name        = var.aws_key_pair
+  keypair_name        = local.aws_key_pair
   deployment_template = var.deployment_template
 
   # From pre-reqs module output
@@ -83,4 +83,65 @@ terraform {
   }
 }
 data "cdp_environments_aws_credential_prerequisites" "cdp_prereqs" {}
+
+
+# ------- Create SSH Keypair if input aws_key_pair variable is not specified
+locals {
+  # flag to determine if keypair should be created
+  create_keypair = var.aws_key_pair == null ? true : false
+
+  # key pair value
+  aws_key_pair = (
+    local.create_keypair == false ?
+    var.aws_key_pair : 
+    aws_key_pair.cdp_keypair[0].key_name
+  )
+}
+
+# Create and save a RSA key
+resource "tls_private_key" "cdp_private_key" {
+  count = local.create_keypair ? 1 : 0
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Save the private key to ./<env_prefix>-ssh-key.pem
+resource "local_sensitive_file" "pem_file" {
+  count = local.create_keypair ? 1 : 0
+
+  filename             = "${var.env_prefix}-ssh-key.pem"
+  file_permission      = "600"
+  directory_permission = "700"
+  content              = tls_private_key.cdp_private_key[0].private_key_pem
+}
+
+# Create an AWS EC2 keypair from the generated public key
+resource "aws_key_pair" "cdp_keypair" {
+  count = local.create_keypair ? 1 : 0
+
+  key_name   = "${var.env_prefix}-keypair"
+  public_key = tls_private_key.cdp_private_key[0].public_key_openssh
+}
+
+# ------- Lookup public ip for ingress settings if ingress_extra_cidrs_and_ports variable is not specified
+locals {
+  # flag to determine if public ip lookup should be performed for ingress
+  lookup_ip = var.ingress_extra_cidrs_and_ports == null ? true : false
+
+  # ingress value
+  ingress_extra_cidrs_and_ports = ( 
+    local.lookup_ip == false ? 
+      var.ingress_extra_cidrs_and_ports : 
+      { cidrs = ["${data.http.my_ip[0].response_body}/32"], 
+        ports = [443, 22]
+      }
+    )
+}
+
+# Perform lookup of public IP of executing host
+data "http" "my_ip" {
+  count = local.lookup_ip ? 1 : 0
+  
+  url = "https://ifconfig.me/ip"
+}
 
