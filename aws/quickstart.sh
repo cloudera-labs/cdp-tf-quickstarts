13a6e3b054b08486083c9146fa4fbb4d11cd5e2b
David Szabo <dszabo@cloudera.com>
2024-10-07 16:19:27 +0200

    CB-27432 azurerm requires subscription ID to be provided explicitly

--- aws/quickstart.sh
+++ aws/quickstart.sh
@@ -19,7 +19,7 @@ export TF_VAR_env_prefix="${2:-""}"
 export ACCOUNT_ID="${3:-""}"
 export CDP_REGION="${4:-"us-west-1"}"
 export TF_VAR_deployment_template="${5:-"semi-private"}"
-export TF_VAR_ingress_extra_cidrs_and_ports="${6:-"{ cidrs = [\"0.0.0.0/0\"], ports = [443, 22] }"}"
+export TF_VAR_ingress_extra_cidrs_and_ports=${6:-"{ cidrs = [\"0.0.0.0/0\"], ports = [443, 22] }"}
 export TF_VAR_env_tags='{"deploy_tool": "express-tf", "env_prefix": "'"$2"'"}'
 export TF_VAR_create_vpc_endpoints="false"
 export TF_VAR_environment_async_creation="true"
@@ -34,11 +34,11 @@ export TF_VAR_aws_region="${TF_VAR_aws_region}"
 export TF_VAR_env_prefix="${TF_VAR_env_prefix}"
 export ACCOUNT_ID="${ACCOUNT_ID}"
 export TF_VAR_deployment_template="${TF_VAR_deployment_template}"
-export TF_VAR_env_tags="${TF_VAR_env_tags}"
+export TF_VAR_env_tags='${TF_VAR_env_tags}'
 export TF_VAR_create_vpc_endpoints="${TF_VAR_create_vpc_endpoints}"
 export TF_VAR_environment_async_creation="${TF_VAR_environment_async_creation}"
 export TF_VAR_datalake_async_creation="${TF_VAR_datalake_async_creation}"
-export TF_VAR_ingress_extra_cidrs_and_ports="${TF_VAR_ingress_extra_cidrs_and_ports}"
+export TF_VAR_ingress_extra_cidrs_and_ports='${TF_VAR_ingress_extra_cidrs_and_ports}'
 export TF_VAR_datalake_scale="${TF_VAR_datalake_scale}"
 EOF
 
@@ -47,9 +47,9 @@ destroy_file="destroy.sh"
 cat <<EOF > $destroy_file
 cd cdp-tf-quickstarts/aws
 
-source variables.sh
+source ${HOME}/variables.sh
 
-terraform destroy -auto-approve
+${HOME}/terraform destroy -auto-approve
 EOF
 
 # Make the file executable
@@ -69,8 +69,11 @@ cd cdp-tf-quickstarts/aws
 pip install cdpcli
 
 config_file="${HOME}/.cdp/config"
-region_config="cdp_region = ${CDP_REGION}"
-echo "$region_config" >> "$config_file"
+mkdir -p "$(dirname "$config_file")"
+cat <<EOF > $config_file
+[default]
+cdp_region = ${CDP_REGION}
+EOF
 
 cdp login --account-id "${ACCOUNT_ID}" --use-device-code
 
--- azure/quickstart.sh
+++ azure/quickstart.sh
@@ -19,11 +19,12 @@ export TF_VAR_env_prefix="${2:-""}"
 export ACCOUNT_ID="${3:-""}"
 export CDP_REGION="${4:-"us-west-1"}"
 export TF_VAR_deployment_template="${5:-"semi-private"}"
-export TF_VAR_ingress_extra_cidrs_and_ports="${6:-"{ cidrs = [\"0.0.0.0/0\"], ports = [443, 22] }"}"
+export TF_VAR_ingress_extra_cidrs_and_ports=${6:-"{ cidrs = [\"0.0.0.0/0\"], ports = [443, 22] }"}
 export TF_VAR_env_tags='{"deploy_tool": "express-tf", "env_prefix": "'"$2"'"}'
 export TF_VAR_environment_async_creation="true"
 export TF_VAR_datalake_async_creation="true"
 export TF_VAR_datalake_scale="LIGHT_DUTY"
+export TF_VAR_azure_subscription_id="$(az account show --query id --output tsv)"
 
 # Save TF variables to file
 output_file="variables.sh"
@@ -33,11 +34,12 @@ export TF_VAR_azure_region="${TF_VAR_azure_region}"
 export TF_VAR_env_prefix="${TF_VAR_env_prefix}"
 export ACCOUNT_ID="${ACCOUNT_ID}"
 export TF_VAR_deployment_template="${TF_VAR_deployment_template}"
-export TF_VAR_env_tags="${TF_VAR_env_tags}"
+export TF_VAR_env_tags='${TF_VAR_env_tags}'
 export TF_VAR_environment_async_creation="${TF_VAR_environment_async_creation}"
 export TF_VAR_datalake_async_creation="${TF_VAR_datalake_async_creation}"
-export TF_VAR_ingress_extra_cidrs_and_ports="${TF_VAR_ingress_extra_cidrs_and_ports}"
+export TF_VAR_ingress_extra_cidrs_and_ports='${TF_VAR_ingress_extra_cidrs_and_ports}'
 export TF_VAR_datalake_scale="${TF_VAR_datalake_scale}"
+export TF_VAR_azure_subscription_id="${TF_VAR_azure_subscription_id}"
 EOF
 
 destroy_file="destroy.sh"
@@ -45,9 +47,9 @@ destroy_file="destroy.sh"
 cat <<EOF > $destroy_file
 cd cdp-tf-quickstarts/azure
 
-source variables.sh
+source ${HOME}/variables.sh
 
-terraform destroy -auto-approve
+${HOME}/terraform destroy -auto-approve
 EOF
 
 # Make the file executable
@@ -55,15 +57,18 @@ chmod +x $output_file
 chmod +x $destroy_file
 
 # Checkout CDP Quickstart Repository
-git clone --branch v0.7.2 https://github.com/cloudera-labs/cdp-tf-quickstarts.git
+git clone --branch v0.8.2 https://github.com/cloudera-labs/cdp-tf-quickstarts.git
 cd cdp-tf-quickstarts/azure
 
 # Install CDP CLI and Log In
 pip install cdpcli
 
 config_file="${HOME}/.cdp/config"
-region_config="cdp_region = ${CDP_REGION}"
-echo "$region_config" >> "$config_file"
+mkdir -p "$(dirname "$config_file")"
+cat <<EOF > $config_file
+[default]
+cdp_region = ${CDP_REGION}
+EOF
 
 cdp login --account-id "${ACCOUNT_ID}" --use-device-code
 
